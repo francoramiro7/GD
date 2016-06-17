899779674e6ae215bbbc9593a4be9b32b920bfbd
alelevy15 <alelevy15@gmail.com>
2016-06-16 22:59:52 -0300

    eliminar rol

--- WindowsFormsApplication1/ABM Rol/Form4.Designer.cs
+++ WindowsFormsApplication1/ABM Rol/Form4.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
@@ -42,15 +50,117 @@
             this.label2.TabIndex = 28;
             this.label2.Text = "Eliminar Rol";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 169);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(531, 39);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Seleccione el Rol que desea eliminar";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MintCream;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1088, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 37);
+            this.button1.TabIndex = 77;
+            this.button1.Text = "✓";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(632, 170);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(333, 42);
+            this.comboBox2.TabIndex = 76;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(288, 321);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(279, 39);
+            this.label3.TabIndex = 78;
+            this.label3.Text = "Se eliminara el Rol";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(590, 321);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 39);
+            this.label4.TabIndex = 79;
+            this.label4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MintCream;
+            this.button5.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(33, 508);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(338, 68);
+            this.button5.TabIndex = 84;
+            this.button5.Text = "Volver";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(478, 508);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(338, 68);
+            this.button2.TabIndex = 85;
+            this.button2.Text = "Continuar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MintCream;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(920, 508);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(338, 68);
+            this.button3.TabIndex = 86;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SlateGray;
             this.ClientSize = new System.Drawing.Size(1326, 1017);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Name = "Form4";
             this.Text = "Eliminar Rol";
+            this.Load += new System.EventHandler(this.Form4_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +169,13 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- WindowsFormsApplication1/ABM Rol/Form4.cs
+++ WindowsFormsApplication1/ABM Rol/Form4.cs
@@ -7,14 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+
 
 namespace WindowsFormsApplication1.ABM_Rol
 {
     public partial class Form4 : Form
     {
+        SqlConnection coneccion;
+        SqlDataReader data;
+        SqlCommand cargarRoles, eliminar;
+
         public Form4()
         {
             InitializeComponent();
+
+            coneccion = new SqlConnection(@"Data Source=localhost\SQLSERVER2012;Initial Catalog=GD1C2016;Persist Security Info=True;User ID=gd;Password=gd2016");
+            coneccion.Open();
+            cargarRoles = new SqlCommand("PERSISTIENDO.cargarRoles", coneccion);
+
+            cargarRoles.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cargarRoles);
+            DataTable tablaRoles = new DataTable();
+
+            coneccion.Close();
+            adapter.Fill(tablaRoles);
+            comboBox2.DataSource = tablaRoles;
+            comboBox2.DisplayMember = "Rol_nombre";
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+            label4.Visible = true;
+            label4.Text = comboBox2.Text.ToString();
+            button2.Visible = true;
+            button3.Visible = true;
+
+            
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //coneccion.Open();
+            //eliminar = new SqlCommand("PERSISTIENDO.eliminarRol", coneccion);
+            //eliminar.CommandType = CommandType.StoredProcedure;
+            //eliminar.Parameters.Add("@nombre", SqlDbType.VarChar).Value = comboBox2.Text.ToString();
+            
+            //eliminar.ExecuteNonQuery();
+            //coneccion.Close();
+
+            //ELIMINAR PRIMERO FUNCIONALIDADES Y LUEGO ROL
+
+
+            String mensaje = "El rol se ha eliminado exitosamente";
+            String caption = "Rol eliminado";
+            MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ABM_Rol.Form1 form1 = new ABM_Rol.Form1();
+            this.Close();
+            form1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+            label4.Visible = false;
+            label4.Text = string.Empty;
+            button2.Visible = false;
+            button3.Visible = false;
         }
     }
 }
