4ae945e1ecf6d9ca0f04104ee078e6ab9ee2b479
francoramiro7 <francoramiro7@gmail.com>
2016-06-25 17:53:32 -0300

    Termino Facturas

--- WindowsFormsApplication1/Facturas/Form1.Designer.cs
+++ WindowsFormsApplication1/Facturas/Form1.Designer.cs
@@ -28,11 +28,300 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.botonUltimaPagina = new System.Windows.Forms.Button();
+            this.botonPaginaSiguiente = new System.Windows.Forms.Button();
+            this.botonPaginaAnterior = new System.Windows.Forms.Button();
+            this.botonPrimeraPagina = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descripcion:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Importe:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(91, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 262);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(94, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(128, 262);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(94, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(110, 262);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "a";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(110, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "a";
+            this.label6.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 360);
+            this.dateTimePicker1.MaxDate = new System.DateTime(3000, 12, 31, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2007, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(94, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.Value = new System.DateTime(2016, 6, 25, 16, 28, 30, 0);
+            this.dateTimePicker1.Visible = false;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(128, 361);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(94, 20);
+            this.dateTimePicker2.TabIndex = 11;
+            this.dateTimePicker2.Visible = false;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MediumAquamarine;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(249, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(623, 409);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(434, 445);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(4, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 26);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Atrás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // botonUltimaPagina
+            // 
+            this.botonUltimaPagina.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonUltimaPagina.Location = new System.Drawing.Point(807, 445);
+            this.botonUltimaPagina.Name = "botonUltimaPagina";
+            this.botonUltimaPagina.Size = new System.Drawing.Size(67, 25);
+            this.botonUltimaPagina.TabIndex = 17;
+            this.botonUltimaPagina.Text = "Ultima";
+            this.botonUltimaPagina.UseVisualStyleBackColor = true;
+            this.botonUltimaPagina.Click += new System.EventHandler(this.botonUltimaPagina_Click);
+            // 
+            // botonPaginaSiguiente
+            // 
+            this.botonPaginaSiguiente.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonPaginaSiguiente.Location = new System.Drawing.Point(735, 445);
+            this.botonPaginaSiguiente.Name = "botonPaginaSiguiente";
+            this.botonPaginaSiguiente.Size = new System.Drawing.Size(66, 25);
+            this.botonPaginaSiguiente.TabIndex = 16;
+            this.botonPaginaSiguiente.Text = "Siguiente";
+            this.botonPaginaSiguiente.UseVisualStyleBackColor = true;
+            this.botonPaginaSiguiente.Click += new System.EventHandler(this.botonPaginaSiguiente_Click);
+            // 
+            // botonPaginaAnterior
+            // 
+            this.botonPaginaAnterior.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonPaginaAnterior.Location = new System.Drawing.Point(664, 445);
+            this.botonPaginaAnterior.Name = "botonPaginaAnterior";
+            this.botonPaginaAnterior.Size = new System.Drawing.Size(65, 25);
+            this.botonPaginaAnterior.TabIndex = 15;
+            this.botonPaginaAnterior.Text = "Anterior";
+            this.botonPaginaAnterior.UseVisualStyleBackColor = true;
+            this.botonPaginaAnterior.Click += new System.EventHandler(this.botonPaginaAnterior_Click);
+            // 
+            // botonPrimeraPagina
+            // 
+            this.botonPrimeraPagina.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonPrimeraPagina.Location = new System.Drawing.Point(594, 444);
+            this.botonPrimeraPagina.Name = "botonPrimeraPagina";
+            this.botonPrimeraPagina.Size = new System.Drawing.Size(64, 26);
+            this.botonPrimeraPagina.TabIndex = 14;
+            this.botonPrimeraPagina.Text = "Primera";
+            this.botonPrimeraPagina.UseVisualStyleBackColor = true;
+            this.botonPrimeraPagina.Click += new System.EventHandler(this.botonPrimeraPagina_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(158, 412);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 26);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(70, 325);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 21;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(884, 482);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.botonUltimaPagina);
+            this.Controls.Add(this.botonPaginaSiguiente);
+            this.Controls.Add(this.botonPaginaAnterior);
+            this.Controls.Add(this.botonPrimeraPagina);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Facturas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button botonUltimaPagina;
+        private System.Windows.Forms.Button botonPaginaSiguiente;
+        private System.Windows.Forms.Button botonPaginaAnterior;
+        private System.Windows.Forms.Button botonPrimeraPagina;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- WindowsFormsApplication1/Facturas/Form1.cs
+++ WindowsFormsApplication1/Facturas/Form1.cs
@@ -7,14 +7,332 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApplication1.Facturas
 {
     public partial class Form1 : Form
     {
+        int pag = 0;
+        int pagTotal;
+        int totalRows = 0;
+
+        SqlConnection coneccion;
+        SqlCommand comprasRealizadas;
+        SqlDataReader data;
+        DataTable tabla = new DataTable();
+
+
         public Form1()
         {
             InitializeComponent();
+
+            coneccion = new SqlConnection(@"Data Source=localhost\SQLSERVER2012;Initial Catalog=GD1C2016;Persist Security Info=True;User ID=gd;Password=gd2016");
+            coneccion.Open();
+
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WindowsFormsApplication1.Form2 form2 = new WindowsFormsApplication1.Form2();
+            form2.Show();
+            this.Close();
+        }
+
+        private void botonPrimeraPagina_Click(object sender, EventArgs e)
+        {
+            pag = 0;
+            actualizarTabla();
+        }
+
+        private void botonPaginaAnterior_Click(object sender, EventArgs e)
+        {
+            if (pag > 0)
+            {
+                pag = pag - 1;
+                actualizarTabla();
+
+            }
+        }
+
+        private void botonPaginaSiguiente_Click(object sender, EventArgs e)
+        {
+            if (pag != pagTotal)
+            {
+                pag++;
+                actualizarTabla();
+
+            }
+        }
+
+        private void botonUltimaPagina_Click(object sender, EventArgs e)
+        {
+            pag = pagTotal;
+            actualizarTabla();
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buscar();
+        }
+
+        private void actualizarTabla()
+        {
+            int ini = pag * 10;
+
+            int n = 0;
+
+            DataTable temporal = tabla.Clone();
+
+
+            dataGridView1.ClearSelection();
+            temporal.Clear();
+
+            while (ini < totalRows && n < 10)
+            {
+
+
+                DataRow row;
+                row = temporal.NewRow();
+                row.ItemArray = tabla.Rows[ini].ItemArray;
+
+                DataRow row2 = tabla.Rows[ini];
+                temporal.Rows.Add(row);
+                ini++;
+                n++;
+
+            }
+            dataGridView1.DataSource = temporal;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            label7.Text = "Pagina " + pag.ToString() + " de " + pagTotal.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buscar();  
+        }
+
+
+        public bool validar()
+        {
+
+            if (String.IsNullOrEmpty(textBox3.Text))
+            {
+                if (!String.IsNullOrEmpty(textBox4.Text))
+                {
+                    String mensaje = "No puede haber solo un importe vacio";
+                    String caption = "Error al ingresar datos";
+                    MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+                    return false;
+                }
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(textBox4.Text))
+                {
+                    String mensaje = "No puede haber solo un importe vacio";
+                    String caption = "Error al ingresar datos";
+                    MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+                    return false;
+                }
+            }
+
+
+            if ((!String.IsNullOrEmpty(textBox4.Text)) && (!String.IsNullOrEmpty(textBox3.Text)))
+            {
+                float imp1= (float) Double.Parse(textBox3.Text);
+                float imp2 =(float)Double.Parse(textBox4.Text);
+                if (imp2 < imp1)
+                {
+                    String mensaje = "El segundo importe debe ser mayor o igual al primero";
+                    String caption = "Error al ingresar datos";
+                    MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            String ingresado = ((TextBox)sender).Text;
+
+            if (esNumero(ingresado, true))
+            {
+            }
+            else
+            {
+                String mensaje = "Solo se pueden ingresar numeros y ',' en este campo";
+                String caption = "Error al ingresar datos";
+                MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+                textBox3.Text = "";
+            }
+        }
+
+        private bool esNumero(String ingresado, bool tieneComa)
+        {
+
+            char[] ingre = ingresado.ToCharArray();
+            int comas = 0;
+            for (int i = 0; i < ingresado.Length; i++)
+            {
+
+                if (ingre[0].Equals(','))
+                {
+                    return false;
+                }
+
+                if (!char.IsNumber(ingre[i]))
+                {
+                    if (tieneComa)
+                    {
+                        if ((!ingre[i].Equals(',')) || (comas > 0))
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            comas++;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+
+                }
+            }
+            return true;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            String ingresado = ((TextBox)sender).Text;
+
+            if (esNumero(ingresado, true))
+            {
+            }
+            else
+            {
+                String mensaje = "Solo se pueden ingresar numeros y ',' en este campo";
+                String caption = "Error al ingresar datos";
+                MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+                textBox4.Text = "";
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.Value = dateTimePicker1.Value;
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            if (dateTimePicker2.Value < dateTimePicker1.Value)
+            {
+                String mensaje = "Debe seleccionar una fecha mayor a la inicial";
+                String caption = "Error al ingresar datos";
+                MessageBox.Show(mensaje, caption, MessageBoxButtons.OK);
+                dateTimePicker2.Value = dateTimePicker1.Value;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = true;
+                label6.Visible = true;
+
+            }
+            else
+            {
+                dateTimePicker1.Visible = false;
+                dateTimePicker2.Visible = false;
+                label6.Visible = false;
+            }
+        }
+    
+        private void buscar()
+        {
+            string parametroImporte = "";
+            string parametroUsuario = "";
+            string parametroDetalle = "";
+            string parametroFechas = "";
+
+            if (validar())
+            {
+                if ((!String.IsNullOrEmpty(textBox4.Text)) && (!String.IsNullOrEmpty(textBox3.Text)))
+                {
+                    float imp1 = (float)Double.Parse(textBox3.Text);
+                    float imp2 = (float)Double.Parse(textBox4.Text);
+
+                    parametroImporte = " and Item_Factura_monto between " + imp1.ToString() + " and " + imp2.ToString();
+
+                }
+
+                if (!String.IsNullOrEmpty(textBox1.Text))
+                {
+                    parametroUsuario = " and Publicacion_vendedor = '" + textBox1.Text + "'";
+
+                }
+
+                if (!String.IsNullOrEmpty(textBox2.Text))
+                {
+                    parametroDetalle = " and Item_Factura_detalle like '%"+textBox2.Text+"%'";
+
+                }
+
+                if (checkBox1.Checked)
+                {
+                    parametroFechas = " and Factura_fecha between '" + dateTimePicker1.Value.ToString() + "' and '" + dateTimePicker2.Value.ToString()+"'";
+                }
+
+                String query = "select Item_Factura_codigo,Item_Factura_detalle,Item_Factura_monto,Factura_fecha" +
+" from PERSISTIENDO.Publicacion" +
+" Join PERSISTIENDO.Factura on Factura_codigo_publicacion = Publicacion_codigo" +
+" Join PERSISTIENDO.Item_Factura On Factura_numero = Item_Factura_codigo_factura" +
+" where Item_Factura_codigo > 0"+parametroUsuario+parametroDetalle+parametroImporte+parametroFechas+
+" order by Item_Factura_codigo desc";
+
+
+
+                SqlCommand comando = new SqlCommand(query, coneccion);
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = comando;
+                tabla.Clear();
+                adapter.Fill(tabla);
+
+                totalRows = tabla.Rows.Count;
+                float cant = (tabla.Rows.Count / 10);
+
+                pagTotal = (int)cant;
+
+                if ((cant - ((int)cant)) != 0)
+                {
+                    pagTotal++;
+                }
+
+                actualizarTabla();
+            }
+
         }
     }
 }
